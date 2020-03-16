0e135b11764d14ba725ff86e42699d8fd90123bf
newt-n94 <newt.n94@gmail.com>
2020-03-16 13:23:43 -0700

    Made water vapor from 50 to 0% by default when initializing hitran_object

--- ReadData.py
+++ ReadData.py
@@ -58,7 +58,7 @@ class Measurement(CombData):
 
     def GetMeasurement(self  ,measurement_number ,dataset_object ):
         self.pressure = dataset_object.pressure[measurement_number]
-        self.FC = dataset_object.FC[ measurement_number ]
+        self.FC = dataset_object.FC[ : ,measurement_number ]
         self.temperature = dataset_object.temperature[ measurement_number ] # degrees K
         self.spectral_grid = dataset_object.wavenumber_grid
         self.min_wavelength = self.spectral_grid[0]
@@ -67,22 +67,36 @@ class Measurement(CombData):
         self.pathlength = dataset_object.pathlength
         return self
 
-    def GetVCD(self):
+    def GetVCD(self ,VMR_H2O = None ,specific_humidity = None):
         Rd = 287.04 # specific gas constant for dry air
         R_universal = 8.314472;
         Na = 6.0221415e23;
         dz = self.pathlength
         temperature = self.temperature
         pressure = self.pressure
+        HumidityToVMR = 1.6068
 
-        rho_n =  pressure/(R_universal*temperature)*Na/10000.0
-        vcd = rho_n * dz
-        return vcd
+
+        if VMR_H2O == None:
+            print('No a priori humidity value was given. Assuming 0 percent humidity by default \n')
+            VMR_H2O = 0.0
+
+        try:
+            VMR_H2O = specific_humidity * HumidityToVMR # convert from specific humidity to volume mixing ratio
+            print('Specific Humidity was given. Converting to Volume Mixing Ratio \n')
+            
+        except:
+            print('Water vapor Volume Mixing Ratio was given. Computing dry air mole fraction \n')
+            
+#        rho_n =  pressure *(1- VMR_H2O ) * 100. /(R_universal*temperature)*Na/10000.0
+        rho_n =  pressure *(1- VMR_H2O ) * 100. /(R_universal*temperature)*Na/10000.0
+        self.VCD = rho_n * dz
+        return self
 
 
     def __init__(self, measurement_number, dataset_object):
         self.GetMeasurement( measurement_number ,dataset_object)
-        self.VCD = self.GetVCD()
+        self.GetVCD()
 
 class HitranSpectra:
     
@@ -92,8 +106,8 @@ class HitranSpectra:
         min_wavelength =   self.min_wavelength # convert to cm^-1
         max_wavelength = self.max_wavelength # convert to bbcm^-1
         wavenumber_resolution = self.spectral_resolution
-        print(max_wavelength, '\n')
-        print(min_wavelength, '\n')
+
+
         self.grid, self.CH4 = absorptionCoefficient_Voigt(SourceTables='CH4_S', WavenumberRange=[ min_wavelength ,max_wavelength ] ,WavenumberStep = wavenumber_resolution ,Environment={'p':pressure_ ,'T':temperature_},IntensityThreshold=1e-27)
         nu_, self.CO2 = absorptionCoefficient_Voigt(SourceTables='CO2_S', WavenumberRange=[ min_wavelength ,max_wavelength ] ,WavenumberStep = wavenumber_resolution ,Environment={'p':pressure_ ,'T':temperature_},IntensityThreshold=1e-27)
         nu_, self.H2O = absorptionCoefficient_Voigt(SourceTables='H2O_S', WavenumberRange=[ min_wavelength ,max_wavelength ] ,WavenumberStep = wavenumber_resolution ,Environment={'p':pressure_ ,'T':temperature_},IntensityThreshold=1e-27)
@@ -104,7 +118,7 @@ class HitranSpectra:
         c = 299792458 * 100 # speed of light
         solar_grid = 1e7 / sun[:,0]
         f_solar = interp1d(solar_grid, sun[:,1])
-        print(solar_grid)
+
         solar_transmission = f_solar(self.grid) # will need to modify this from nm to cm^-1
         return solar_transmission
 
@@ -123,9 +137,4 @@ class HitranSpectra:
 
 
 
-file = 'testdata_2.h5'
-data = CombData(file)
-current_data = Measurement( 100 , data)
-GetCrossSections( current_data.min_wavelength ,  current_data.max_wavelength)
-spectra = HitranSpectra(current_data)
 
